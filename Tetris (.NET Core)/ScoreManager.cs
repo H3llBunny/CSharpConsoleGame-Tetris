a71b1d0ef2e94af544919edc61b1b211fa1cf6e2
H3llBunny <bogo.dishlyanov@gmail.com>
2023-09-10 00:36:36 +0300

    Updated and bug fix

---
+++ Tetris (.NET Core)/ScoreManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Tetris
+{
+    public class ScoreManager
+    {
+        private readonly string highScoreFile;
+
+        public ScoreManager(string highScoreFile)
+        {
+            this.highScoreFile = highScoreFile;
+            this.HighScore = this.GetHihgScore();
+        }
+
+        public int Score { get; private set; }
+
+        public int HighScore { get; private set; }
+
+        public void AddToScore(int addToScore)
+        {
+            this.Score += addToScore;
+
+            if (this.Score > this.HighScore)
+            {
+                this.HighScore = this.Score;
+            }
+        }
+
+        public void AddToHighScore()
+        {
+            File.AppendAllLines(this.highScoreFile, new List<string>
+            {
+                            $"[{DateTime.Now.ToString()}] {Environment.UserDomainName} => {this.Score}"
+                        });
+        }
+
+        private int GetHihgScore()
+        {
+            var highScore = 0;
+
+            var allScores = File.ReadAllLines(this.highScoreFile);
+
+            foreach (var score in allScores)
+            {
+                var match = Regex.Match(score, @" => (?<score>[0-9]+)");
+                highScore = Math.Max(highScore, int.Parse(match.Groups["score"].Value));
+            }
+
+            return highScore;
+        }
+    }
+}
